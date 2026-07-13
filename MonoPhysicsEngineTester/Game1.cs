f9f954f1e92a716475415e07b4a459ebffa8358a
Kr0issant <93475036+Kr0issant@users.noreply.github.com>
2026-07-13 18:14:13 +0530

    update code for new camera zoom logic

--- MonoPhysicsEngineTester/Game1.cs
+++ MonoPhysicsEngineTester/Game1.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
@@ -7,6 +9,7 @@ using MonoUtils.Graphics;
 using MonoUtils.Input;
 using MonoUtils.Utility;
 using MonoPhysicsEngine;
+using MonoPhysicsEngine.Content;
 
 namespace MonoPhysicsEngineTester;
 
@@ -24,7 +27,12 @@ public class Game1 : Game
     private const int SCREEN_WIDTH = 1280;
     private const int SCREEN_HEIGHT = 720;
 
-    private float x = 0;
+    private const int DEFAULT_ZOOM = 4;
+
+    private Color SHAPE_BORDER_COLOR = Color.Black;
+    
+    private List<RigidBody> bodies;
+    private List<Color> colors;
 
     public Game1()
     {
@@ -47,6 +55,52 @@ public class Game1 : Game
         sprites = new Sprites(this);
         screen = new Screen(this, SCREEN_WIDTH, SCREEN_HEIGHT);
         camera = new Camera(screen);
+
+        camera.Zoom = DEFAULT_ZOOM;
+        camera.GetScreenBounds(out float left, out float right, out float bottom, out float top);
+        Console.WriteLine(left);
+        Console.WriteLine(right);
+        Console.WriteLine(top);
+        Console.WriteLine(bottom);
+        
+        float padding = 20f;
+        int numBodies = 10;
+        bodies = new List<RigidBody>(numBodies);
+        colors = new List<Color>(numBodies);
+        Random rng = new Random();
+        
+        for (int i = 0; i < numBodies; i++)
+        {
+            RigidBody body = null;
+            string msg = null;
+            bool success = false;
+            
+            int type = rng.Next(0, 2);
+            // int type = (int)ShapeType.Circle;
+            
+            int x = rng.Next((int)(left + padding), (int)(right - padding));
+            int y = rng.Next((int)(bottom + padding), (int)(top - padding));
+            Color color = new Color((float)rng.NextDouble(), (float)rng.NextDouble(), (float)rng.NextDouble());
+            double area = rng.NextDouble() * (World.MaxBodySize - World.MinBodySize) + World.MinBodySize;
+            float density = (float)(rng.NextDouble() * (World.MaxDensity - World.MinDensity) + World.MinDensity);
+            
+            if (type == 0)
+            {
+                float radius = MathF.Sqrt((float)area / MathF.PI);
+                success = RigidBody.CreateCircleBody(new MonoVector(x, y), radius, density, false, 1f, out body, out msg);
+            }
+            else if (type == 1)
+            {
+                float maxDimen = MathF.Sqrt((float)area);
+                float width = maxDimen * (float)(rng.NextDouble() * 1.0 + 0.5);
+                float height = (float)area / width;
+        
+                success = RigidBody.CreateBoxBody(new MonoVector(x, y), width, height, density, false, 1f, out body, out msg);
+            }
+            
+            if (success) { bodies.Add(body); colors.Add(color); }
+            else Console.WriteLine($"[{i}] {msg}");
+        }
         
         base.Initialize();
     }
@@ -62,15 +116,22 @@ public class Game1 : Game
         keyboard.Update();
         mouse.Update();
         
+        // Console.WriteLine(camera.Z);
+        float zoomFactor = (float)(camera.Z / camera.ZBase);
+        
         if (keyboard.IsKeyClicked(Keys.Escape)) { Exit(); }
         if (keyboard.IsKeyClicked(Keys.F)) { screen.ToggleFullScreen(graphics); }
         
-        if (mouse.IsMiddleButtonDown()) { camera.MoveCam(new Vector2(-mouse.DeltaX * camera.ZoomFactor, mouse.DeltaY * camera.ZoomFactor)); }
-        if (mouse.IsScrollingUp()) { camera.MoveZ(35f * camera.ZoomFactor); }
-        if (mouse.IsScrollingDown()) { camera.MoveZ(-35f * camera.ZoomFactor); }
-
-
-        x += 0.05f;
+        if (mouse.IsMiddleButtonDown()) { camera.MoveBy(new Vector2(-mouse.DeltaX, mouse.DeltaY) * zoomFactor); }
+        if (mouse.IsScrollingUp()) { camera.MoveZ(35f * zoomFactor); }
+        if (mouse.IsScrollingDown()) { camera.MoveZ(-35f * zoomFactor); }
+        
+        if (keyboard.IsKeyDown(Keys.Left)) { camera.MoveBy(new Vector2(-8f, 0f) * zoomFactor); }
+        if (keyboard.IsKeyDown(Keys.Right)) { camera.MoveBy(new Vector2(8f, 0f) * zoomFactor); }
+        if (keyboard.IsKeyDown(Keys.Up)) { camera.MoveBy(new Vector2(0f, 8f) * zoomFactor); }
+        if (keyboard.IsKeyDown(Keys.Down)) { camera.MoveBy(new Vector2(0f, -8f) * zoomFactor); }
+        if (keyboard.IsKeyClicked(Keys.R)) { camera.ResetZ(); camera.MoveBy(-camera.Position); }
+        
         base.Update(gameTime);
     }
 
@@ -83,11 +144,23 @@ public class Game1 : Game
         sprites.End();
         
         shapes.Begin(camera);
-        // shapes.DrawRectangle(20f, 0f, 100f, -50f, Color.Red);
-        // shapes.DrawRectangle(20f, 0f, 100f, -50f, Color.Black, Shapes.FillMode.Border, 4f);
-        // shapes.DrawCircle(new Vector2(0f, 0f), 50f, Color.White);
-        // shapes.DrawRegularPolygon(new Transform2D(new Vector2(0f, 0f), new Vector2(50, 50), x), 5, Color.Red);
-        shapes.DrawLine(Vector2.Zero, new MonoVector(200, 150).ToVector2(), 2f, Color.Red);
+        for (int i = 0; i < bodies.Count; i++)
+        {
+            RigidBody body = bodies[i];
+            ShapeType type = body.ShapeType;
+            if (type == ShapeType.Circle)
+            {
+                shapes.DrawCircle(body.Position.ToVector2(), body.Radius, colors[i], Shapes.FillMode.Filled);
+                shapes.DrawCircle(body.Position.ToVector2(), body.Radius, SHAPE_BORDER_COLOR, Shapes.FillMode.Border);
+            }
+            else if (type == ShapeType.Box)
+            {
+                shapes.DrawRectangle(body.Position.ToVector2(), body.Width, body.Height, colors[i], Shapes.FillMode.Filled);
+                shapes.DrawRectangle(body.Position.ToVector2(), body.Width, body.Height, SHAPE_BORDER_COLOR, Shapes.FillMode.Border);
+            }
+        }
+        // shapes.DrawLine(Vector2.Zero, new MonoVector(2f, 1f).ToVector2(), 0.01f, Color.White);
+        // shapes.DrawCircle();
         shapes.End();
         
         screen.Unset();
