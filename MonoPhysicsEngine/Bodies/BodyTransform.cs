14ecc437e3b427e98f0591b3a6803a3e637db493
Kr0issant <93475036+Kr0issant@users.noreply.github.com>
2026-07-13 22:39:57 +0530

    forgot to add files

---
+++ MonoPhysicsEngine/Bodies/BodyTransform.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MonoPhysicsEngine;
+
+internal readonly struct BodyTransform
+{
+    public readonly MonoVector Position;
+    public readonly float Sin;
+    public readonly float Cos;
+    
+    public readonly static BodyTransform Zero = new BodyTransform(0f, 0f, 0f);
+
+    public BodyTransform(MonoVector position, float angle)
+    {
+        Position = new MonoVector(position.X, position.Y);
+        Sin = MathF.Sin(angle);
+        Cos = MathF.Cos(angle);
+    }
+    public BodyTransform(float x, float y, float angle)
+    {
+        Position = new  MonoVector(x, y);
+        Sin = MathF.Sin(angle);
+        Cos = MathF.Cos(angle);
+    }
+}
---
+++ MonoPhysicsEngine/Util.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace MonoPhysicsEngine;
+
+public static class Util
+{
+    public static void ToVector2Array(MonoVector[] src, ref Vector2[] dst)
+    {
+        if (dst is null || src.Length != dst.Length)
+        {
+            dst = new Vector2[src.Length];
+        }
+
+        for (int i = 0; i < src.Length; i++)
+        {
+            dst[i] = src[i].ToVector2();
+        }
+    }
+}
