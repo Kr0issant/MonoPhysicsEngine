bfc0cf86469829fd81336478f9e232b950a7d1ac
Kr0issant <93475036+Kr0issant@users.noreply.github.com>
2026-07-21 14:42:35 +0530

    separate collision resolution using manifolds

---
+++ MonoPhysicsEngine/Structures/MonoManifold.cs
@@ -0,0 +1,25 @@
+﻿using MonoPhysicsEngine.Content;
+
+namespace MonoPhysicsEngine;
+
+public readonly struct MonoManifold
+{
+    public readonly RigidBody BodyA;
+    public readonly RigidBody BodyB;
+    public readonly MonoVector Normal;
+    public readonly float Depth;
+    public readonly MonoVector Contact1;
+    public readonly MonoVector Contact2;
+    public readonly int ContactCount;
+
+    public MonoManifold(RigidBody bodyA, RigidBody bodyB, MonoVector normal, float depth, MonoVector contact1, MonoVector contact2, int contactCount)
+    {
+        BodyA = bodyA;
+        BodyB = bodyB;
+        Normal = normal;
+        Depth = depth;
+        Contact1 = contact1;
+        Contact2 = contact2;
+        ContactCount = contactCount;
+    }
+}
--- MonoPhysicsEngine/World.cs
+++ MonoPhysicsEngine/World.cs
@@ -14,6 +14,9 @@ public sealed class World
     public static readonly float MinDensity = 0.5f; // g/cm3
     public static readonly float MaxDensity = 25f;
 
+    public static readonly int MinIterations = 1;
+    public static readonly int MaxIterations = 128;
+
     public static readonly float SCREEN_SPAWN_PADDING = 20f;
 
     public int BodyCount => bodies.Count;
@@ -22,12 +25,15 @@ public sealed class World
     
     private List<RigidBody> bodies;
     private Vector2[] vertexBuffer;
+    private List<MonoManifold> contacts;
 
     public World()
     {
-        gravity = new MonoVector(0, -981f);
+        // gravity = new MonoVector(0, -981f);
+        gravity = new MonoVector(0, -600f);
 
         bodies = new List<RigidBody>();
+        contacts = new List<MonoManifold>();
     }
 
     public void AddBody(RigidBody body)
@@ -46,37 +52,66 @@ public sealed class World
         throw new IndexOutOfRangeException("Body index out of range.");
     }
 
-    public void Step(float deltaTime)
+    public void Step(float deltaTime, int iterations = 24)
     {
-        /* --- Movement --- */
-        for (int i = 0; i < bodies.Count; i++)
+        iterations = Math.Clamp(iterations, MinIterations, MaxIterations);
+        float subStepDt = deltaTime / iterations;
+
+        for (int t = 0; t < iterations; t++)
         {
-            if (!bodies[i].IsStatic) bodies[i].AddForce(gravity * bodies[i].Mass);
-            bodies[i].Step(deltaTime);
-        }
-        
-        /* --- Collision Detection and Resolution --- */
-        
-        for (int i = 0; i < bodies.Count; i++)
-        {
-            RigidBody bodyA = bodies[i];
-            for (int j = i + 1; j < bodies.Count; j++)
+            /* --- Movement --- */
+            for (int i = 0; i < bodies.Count; i++)
             {
-                RigidBody bodyB = bodies[j];
-
-                if (bodyA.IsStatic && bodyB.IsStatic) continue;
-
-                if (Collisions.CheckGeneralCollision(bodyA, bodyB, out MonoVector normal, out float depth))
+                if (!bodies[i].IsStatic) { bodies[i].AddForce(gravity * bodies[i].Mass); }
+                bodies[i].Step(subStepDt);
+            }
+            
+            /* --- Collision Detection --- */
+            
+            for (int i = 0; i < bodies.Count; i++)
+            {
+                for (int j = i + 1; j < bodies.Count; j++)
                 {
-                    if (!bodyA.IsStatic) bodyA.MoveBy(normal * depth * (bodyB.Mass / (bodyA.Mass + bodyB.Mass)));
-                    if (!bodyB.IsStatic) bodyB.MoveBy(-normal * depth * (bodyA.Mass / (bodyA.Mass + bodyB.Mass)));
-                    Collisions.ResolveCollision(bodyA, bodyB, normal, depth);
+                    RigidBody bodyA = bodies[i];
+                    RigidBody bodyB = bodies[j];
+
+                    if (bodyA.IsStatic && bodyB.IsStatic) continue;
+
+                    if (Collisions.CheckGeneralCollision(bodyA, bodyB, out MonoVector normal, out float depth))
+                    {
+                        float totalInvMass = bodyA.InvMass + bodyB.InvMass;
+                        if (totalInvMass > 0f)
+                        {
+                            // const float percent = 0.4f;
+                            // const float slop = 0.03f;
+                            
+                            // float correctionMagnitude = Math.Max(depth - slop, 0.0f) / totalInvMass * percent;
+                            float correctionMagnitude = depth / totalInvMass;
+                            MonoVector correctionVector = normal * correctionMagnitude;
+                            
+                            if (!bodyA.IsStatic) bodyA.MoveBy(correctionVector * bodyA.InvMass);
+                            if (!bodyB.IsStatic) bodyB.MoveBy(-correctionVector * bodyB.InvMass);
+                        }
+
+                        MonoManifold contact = new MonoManifold(bodyA, bodyB, normal, depth, MonoVector.Zero, MonoVector.Zero, 0);
+                        contacts.Add(contact);
+                        
+                        // Collisions.ResolveCollision(bodyA, bodyB, normal, depth);
+                    }
                 }
+            }
+
+            /* --- Collision Resolution --- */
+            for (int i = contacts.Count - 1; i >= 0; i--)
+            {
+                MonoManifold contact = contacts[i];
+                Collisions.ResolveCollision(in contact);
+                contacts.RemoveAt(i);
             }
         }
     }
 
-    public void DrawShapes(Shapes shapes, World world)
+    public void DrawShapes(Shapes shapes, World world, bool renderAABBs)
     {
         for (int i = 0; i < world.BodyCount; i++)
         {
@@ -94,6 +129,12 @@ public sealed class World
                 Util.ToVector2Array(body.GetTransformedVertices(), ref vertexBuffer);
                 if (fill) shapes.DrawPolygon(vertexBuffer, body.Triangles, body.FillColor, Shapes.FillMode.Filled);
                 shapes.DrawPolygon(vertexBuffer, body.Triangles, body.BorderColor, Shapes.FillMode.Border);
+            }
+            if (renderAABBs)
+            {
+                MonoAABB box = body.GetAABB();
+                Vector2 center = ((box.Min + box.Max) / 2f).ToVector2();
+                shapes.DrawRectangle(center, box.Max.X - box.Min.X, box.Max.Y - box.Min.Y, Color.White, Shapes.FillMode.Border);
             }
         }
     }
