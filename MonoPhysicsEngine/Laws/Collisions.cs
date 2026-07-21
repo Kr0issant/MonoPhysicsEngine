aeebc653022f8880c83e0fa3a3c178c3ebf70a87
Kr0issant <93475036+Kr0issant@users.noreply.github.com>
2026-07-21 14:49:43 +0530

    get rid of unnecessary collision functions

--- MonoPhysicsEngine/Laws/Collisions.cs
+++ MonoPhysicsEngine/Laws/Collisions.cs
@@ -54,43 +54,6 @@ public static class Collisions
         depth = (radiusA + radiusB) - distance;
         return true;
     }
-
-    public static bool CheckPolygonCollision(MonoVector[] verticesA, MonoVector[] verticesB, out MonoVector normal, out float depth)
-    {
-        normal = MonoVector.Zero;
-        depth = float.MaxValue;
-
-        int totalEdges = verticesA.Length + verticesB.Length;
-        for (int i = 0; i < totalEdges; i++)
-        {
-            MonoVector[] currentShape = i < verticesA.Length ? verticesA : verticesB;
-            int index = i < verticesA.Length ? i : i - verticesA.Length;
-            
-            MonoVector va1 = currentShape[index];
-            MonoVector va2 = currentShape[(index + 1) % currentShape.Length];
-
-            MonoVector edge = va2 - va1;
-            MonoVector axis = new MonoVector(-edge.Y, edge.X).Normalize();
-            
-            Util.ProjectVerticesOnAxis(verticesA, axis, out float minA, out float maxA);
-            Util.ProjectVerticesOnAxis(verticesB, axis, out float minB, out float maxB);
-            
-            if (minA >= maxB || minB >= maxA) return false;
-
-            float axisDepth = MathF.Min(maxA, maxB) - MathF.Max(minA, minB);
-            if (axisDepth < depth)
-            {
-                depth = axisDepth;
-                normal = axis;
-            }
-        }
-
-        MonoVector centerA = Util.GetPolygonCenter(verticesA);
-        MonoVector centerB = Util.GetPolygonCenter(verticesB);
-        if (MonoVector.Dot(normal, centerA - centerB) < 0) { normal = -normal; }
-        
-        return true;
-    }
     
     public static bool CheckPolygonCollision(MonoVector centerA, MonoVector[] verticesA, MonoVector centerB, MonoVector[] verticesB, out MonoVector normal, out float depth)
     {
@@ -124,67 +87,6 @@ public static class Collisions
         
         if (MonoVector.Dot(normal, centerA - centerB) < 0) { normal = -normal; }
         
-        return true;
-    }
-
-    public static bool CheckCirclePolygonCollision(MonoVector centerA, float radiusA, MonoVector[] verticesB, out MonoVector normal, out float depth)
-    {
-        normal = MonoVector.Zero;
-        depth = float.MaxValue;
-
-        MonoVector closestVertex = verticesB[0];
-        float minDistance = (closestVertex - centerA).LengthSquared;
-        float distance;
-
-        for (int i = 0; i < verticesB.Length; i++)
-        {
-            MonoVector va1 = verticesB[i];
-            MonoVector va2 = verticesB[(i + 1) % verticesB.Length];
-
-            distance = (va1 - centerA).LengthSquared;
-            if (distance < minDistance)
-            {
-                closestVertex = va1;
-                minDistance = distance;
-            }
-
-            MonoVector edge = va2 - va1;
-            MonoVector axis = new MonoVector(-edge.Y, edge.X).Normalize();
-            
-            Util.ProjectCircleOnAxis(centerA, radiusA, axis, out float minA, out float maxA);
-            Util.ProjectVerticesOnAxis(verticesB, axis, out float minB, out float maxB);
-            
-            if (minA >= maxB || minB >= maxA) return false;
-
-            float axisDepth = MathF.Min(maxA, maxB) - MathF.Max(minA, minB);
-            if (axisDepth < depth)
-            {
-                depth = axisDepth;
-                normal = axis;
-            }
-        }
-
-        MonoVector cornerAxis = centerA - closestVertex;
-        if (cornerAxis.LengthSquared > 0.01f)
-        {
-            cornerAxis = cornerAxis.Normalize();
-
-            Util.ProjectCircleOnAxis(centerA, radiusA, cornerAxis, out float minA, out float maxA);
-            Util.ProjectVerticesOnAxis(verticesB, cornerAxis, out float minB, out float maxB);
-
-            if (minA >= maxB || minB >= maxA) return false;
-
-            float axisDepth = MathF.Min(maxA, maxB) - MathF.Max(minA, minB);
-            if (axisDepth < depth)
-            {
-                depth = axisDepth;
-                normal = cornerAxis;
-            }
-        }
-
-        MonoVector centerB = Util.GetPolygonCenter(verticesB);
-        if (MonoVector.Dot(normal, centerA - centerB) < 0) { normal = -normal; }
-
         return true;
     }
     
@@ -246,22 +148,6 @@ public static class Collisions
         if (MonoVector.Dot(normal, centerA - centerB) < 0) { normal = -normal; }
 
         return true;
-    }
-
-    public static void ResolveCollision(RigidBody bodyA, RigidBody bodyB, MonoVector normal, float depth)
-    {
-        MonoVector relativeVelocity = bodyA.LinearVelocity - bodyB.LinearVelocity;
-
-        if (MonoVector.Dot(relativeVelocity, normal) > 0f) return; // Bodies are already moving apart
-        
-        float e = MathF.Min(bodyA.Restitution, bodyB.Restitution);
-        float j = -(1f + e) * MonoVector.Dot(relativeVelocity, normal);
-        j /= bodyA.InvMass + bodyB.InvMass;
-
-        MonoVector impulse = j * normal;
-        
-        bodyA.LinearVelocity += impulse * bodyA.InvMass;
-        bodyB.LinearVelocity -= impulse * bodyB.InvMass;
     }
     
     public static void ResolveCollision(in MonoManifold contact)
